1b90d8c4d445680144eae5e1a19eb4a290b83ca9
David Vidovic <david.vidovic2000@gmail.com>
2025-01-22 13:25:20 +0100

    Added support for printing characters to the console using OpenSBI API ecall. Better documenated some code segments for better understanding.

--- kernel.c
+++ kernel.c
@@ -1,3 +1,5 @@
+#include "kernel.h"
+
 typedef unsigned char uint8_t;
 typedef unsigned char uint32_t;
 typedef uint32_t size_t;
@@ -15,12 +17,54 @@ void *memset(void *buf, char c, size_t n)
 	return buf;
 }
 
+struct sbiret sbi_call(
+		long arg0, 
+		long arg1,
+		long arg2,
+		long arg3,
+		long arg4,
+		long arg5,
+		long fid,
+		long eid)
+{
+	/* Place arguments into registers */
+	register long a0 __asm__("a0") = arg0;
+	register long a1 __asm__("a1") = arg1;
+	register long a2 __asm__("a2") = arg2;
+	register long a3 __asm__("a3") = arg3;
+	register long a4 __asm__("a4") = arg4;
+	register long a5 __asm__("a5") = arg5;
+	register long a6 __asm__("a6") = fid;
+	register long a7 __asm__("a7") = eid;
+
+	/* By calling ecall instruction, CPU will switch from kernel mode (S-Mode)
+	 * to OpenSBI mode (M-Mode). As it reads 0x01 in a7 (EID), it knows that it needs to
+	 * print to the console whatever is placed in a0 register. */
+	__asm__ __volatile__("ecall"
+			: "=r"(a0), "=r"(a1)
+			: "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+		        : "memory"	
+			);
+	return (struct sbiret){.error = a0, .value = a1};
+}
+
+void putchar(char ch) 
+{
+	/* This is sbi_console_putchar function, which uses 0x01 as EID, hence 1 as the last arg */
+	sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
+}
+
 void kernel_main(void) 
 {
 	// Init __bss section with zeros
 	memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-	for(;;);
+	const char *s = "\n\nHello World!\n";
+	for(int i = 0; s[i] != '\0'; i++)
+		putchar(s[i]);
+
+	for(;;)
+		__asm__ __volatile__("wfi");
 }
 
 
